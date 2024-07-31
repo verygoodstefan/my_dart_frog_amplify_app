f2fbf0e05575070f26ad103c21223cf11dcf8361
Stefan <stefan@verygood.ventures>
2024-07-31 10:23:20 -0400

    more comments marking breadcrumbs

--- api/main.dart
+++ api/main.dart
@@ -8,7 +8,13 @@ import 'amplify_outputs.dart';
 Future<void> init(InternetAddress ip, int port) async {
   final dataSource = AmplifyDataSource();
 
-  // final amplify = AmplifyClass();
+  final amplify = AmplifyClass();
+  //do we need this sort of a setter in the setup?
+  // if so, where does this have to be?
+  // and then, do we have to ensure that we are using
+  // this amplify (AmplifyClass.instance) whenever we
+  // use dependencies?
+  Amplify = amplify;
   // await amplify.addPlugin(dataSource.api);
   // await amplify.configure(amplifyConfig);
 }

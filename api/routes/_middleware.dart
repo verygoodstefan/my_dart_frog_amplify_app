a9cc11d8bb827dbd671331b420772199e136e8f8
Stefan <stefan@verygood.ventures>
2024-08-02 17:22:16 -0400

    opening up auth methods

--- api/routes/_middleware.dart
+++ api/routes/_middleware.dart
@@ -6,14 +6,17 @@ import 'package:dart_frog_auth/dart_frog_auth.dart';
 Handler middleware(Handler handler) {
   return handler
       .use(requestLogger())
-      // .use(
-      //   bearerAuthentication<AuthUser>(
-      //     authenticator: (context, token) async {
-      //       // will need to validate token
-      //       // https://docs.aws.amazon.com/cognito/latest/developerguide/amazon-cognito-user-pools-using-tokens-verifying-a-jwt.html
-      //       return context.read<AmplifyUserRepository>().getCurrentUser();
-      //     },
-      //   ))
+      .use(bearerAuthentication<AuthUser>(
+        authenticator: (context, token) async {
+          // will need to validate token
+          // https://docs.aws.amazon.com/cognito/latest/developerguide/amazon-cognito-user-pools-using-tokens-verifying-a-jwt.html
+          return context.read<AmplifyUserRepository>().getCurrentUser();
+        },
+        applies: (context) async {
+          //need to do something like this to open auth but not other routes
+          return !context.request.uri.pathSegments.contains('auth');
+        },
+      ))
       .use(
     provider<AmplifyUserRepository>((_) {
       return AmplifyUserRepository(auth: Amplify.Auth);
